7610d1901c0721e27e150a4e47d407f7bf8fd1db
Aryadani Christian W <58355682+arydnichrstian@users.noreply.github.com>
2020-10-08 12:55:09 +0700

    Update Parameters_and_Arguments.java

--- Praktikum-Minggu-4/Parameters_and_Arguments.java
+++ Praktikum-Minggu-4/Parameters_and_Arguments.java
@@ -1,7 +1,7 @@
 public class Parameters_and_Arguments {
 	
 	static void myMethod(String fname) {
-		System.out.println(fname + " Uchih ");
+		System.out.println(fname + " Uchiha ");
 	}
 	
 	public static void main(String[] args) {
