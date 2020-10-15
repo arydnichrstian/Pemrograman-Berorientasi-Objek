ed88d523e4be9a1cfc7a9ee4bbff477dad296a87
Aryadani Christian W <58355682+arydnichrstian@users.noreply.github.com>
2020-10-15 12:15:28 +0700

    Add files via upload

---
+++ Praktikum-Minggu-1/String_args.java
@@ -0,0 +1,7 @@
+public class String_args {
+
+    public static void main(String[] args){
+        System.out.println("Hallo " + "Ganteng");
+        System.out.println("Hello " + "Cantik");
+    }
+}
